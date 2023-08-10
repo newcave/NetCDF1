edd4668e7a7cdf9e44fa86b9e786877377f678ca
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-10 20:33:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
-import folium
-import netCDF4 as nc
-import os
 import cartopy.crs as ccrs
+import netCDF4 as nc
 
 # Create a Streamlit app
 st.title('Rainfall Prediction - KMA')
@@ -40,36 +35,43 @@ if uploaded_file is not None:
         # Trim the rain_array to match the shape of trimmed latitude and longitude arrays
         rain_trimmed = rain_array[:latitude_trimmed.shape[0], :latitude_trimmed.shape[1]]
 
-        # Display the heatmap using Matplotlib
-        aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
-        fig = plt.figure(figsize=(6 * aspect_ratio, 6))
-        plt.imshow(rain_trimmed, cmap='rainbow', extent=[longitude_trimmed.min(), longitude_trimmed.max(), latitude_trimmed.min(), latitude_trimmed.max()], vmax=5)
-        plt.colorbar(label='mm/hr')
-        plt.title('Rainfall_Pred_KMA')
-        plt.xlabel('Longitude')
-        plt.ylabel('Latitude')
-
-        # Show Matplotlib heatmap if Cartopy map is not selected
-        if not show_cartopy_map:
-            st.pyplot(fig)
-
-        # Create a Cartopy map if Cartopy map is selected
         if show_cartopy_map:
-            plt.close()  # Close the Matplotlib plot
+            # Calculate aspect ratio
+            aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
+
+            # Create the figure with adjusted size and aspect ratio
             plt.figure(figsize=(10, 10 * aspect_ratio))
+
+            # Create a map using PlateCarree projection
             ax = plt.axes(projection=ccrs.PlateCarree())
+
+            # Plot the heatmap using pcolormesh
             heatmap = plt.pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
+
+            # Add colorbar
             cbar = plt.colorbar(heatmap, label='mm/hr')
+
+            # Set map title and labels
             plt.title('Rainfall Prediction')
             plt.xlabel('Longitude')
             plt.ylabel('Latitude')
+
+            # Add coastlines
             ax.coastlines()
-            buffer = BytesIO()
-            plt.savefig(buffer, format='png')
-            buffer.seek(0)
-            st.image(buffer, caption='Rainfall Prediction Heatmap (Cartopy)', use_column_width=True)
-            plt.close()
-            
+
+            # Show the plot
+            plt.show()
+        else:
+            # Display the heatmap using Matplotlib
+            aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
+            fig = plt.figure(figsize=(6 * aspect_ratio, 6))
+            plt.imshow(rain_trimmed, cmap='rainbow', extent=[longitude_trimmed.min(), longitude_trimmed.max(), latitude_trimmed.min(), latitude_trimmed.max()], vmax=5)
+            plt.colorbar(label='mm/hr')
+            plt.title('Rainfall_Pred_KMA')
+            plt.xlabel('Longitude')
+            plt.ylabel('Latitude')
+            st.pyplot(fig)
+
     except Exception as e:
         st.write("Error during loading:", e)
     finally:
@@ -77,4 +79,3 @@ if uploaded_file is not None:
 
 else:
     st.write("Please upload a NetCDF file using the sidebar.")
-
