1ee1154e88c53b7cc113ab37f630d883acba1ce1
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-10 22:35:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,15 +79,27 @@ if uploaded_file is not None:
             st.pyplot(fig)
 
 
+# Calculate aspect ratio
+            aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
+
+            # Create a map using PlateCarree projection
             fig2 = plt.figure(figsize=(10, 10 / aspect_ratio))
             ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
+            # Plot the heatmap using pcolormesh
             heatmap = ax.pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
+
+            # Add colorbar
             cbar = plt.colorbar(heatmap, ax=ax, label='mm/hr', orientation='vertical')
+
+            # Set map title and labels
             ax.set_title('Rainfall Prediction')
             ax.set_xlabel('Longitude')
             ax.set_ylabel('Latitude')
+
             # Add coastlines
             ax.coastlines()
+
             # Show the plot
             st.pyplot(fig2)
 
