dd02c905b3e26333cf3372b8bac0ed71a0a16665
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-10 21:07:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,28 +44,26 @@ if uploaded_file is not None:
             # Calculate aspect ratio
             aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
 
-            # Create the figure with adjusted size and aspect ratio
-            plt.figure(figsize=(10, 10 * aspect_ratio))
-
             # Create a map using PlateCarree projection
-            ax = plt.axes(projection=ccrs.PlateCarree())
+            fig = plt.figure(figsize=(10, 10 * aspect_ratio))
+            ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
             # Plot the heatmap using pcolormesh
-            heatmap = plt.pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
+            heatmap = ax.pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
 
             # Add colorbar
-            cbar = plt.colorbar(heatmap, label='mm/hr')
+            cbar = plt.colorbar(heatmap, ax=ax, label='mm/hr')
 
             # Set map title and labels
-            plt.title('Rainfall Prediction')
-            plt.xlabel('Longitude')
-            plt.ylabel('Latitude')
+            ax.set_title('Rainfall Prediction')
+            ax.set_xlabel('Longitude')
+            ax.set_ylabel('Latitude')
 
             # Add coastlines
             ax.coastlines()
 
             # Show the plot
-            plt.show()
+            st.pyplot(fig)
         else:
             # Display the heatmap using Matplotlib
             aspect_ratio = (longitude_trimmed.max() - longitude_trimmed.min()) / (latitude_trimmed.max() - latitude_trimmed.min())
