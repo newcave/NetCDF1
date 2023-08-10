99727c0ea7b9d9b1236a4bf609e5e4f00054a344
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-10 22:37:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,21 +84,19 @@ if uploaded_file is not None:
 
             # Create a map using PlateCarree projection
             fig2 = plt.figure(figsize=(10, 10 / aspect_ratio))
-            ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-
             # Plot the heatmap using pcolormesh
-            heatmap = ax.pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
+            heatmap = pcolormesh(longitude_trimmed, latitude_trimmed, rain_trimmed, cmap='rainbow', vmax=5, transform=ccrs.PlateCarree())
 
             # Add colorbar
             cbar = plt.colorbar(heatmap, ax=ax, label='mm/hr', orientation='vertical')
 
             # Set map title and labels
-            ax.set_title('Rainfall Prediction')
-            ax.set_xlabel('Longitude')
-            ax.set_ylabel('Latitude')
+            plt.set_title('Rainfall Prediction')
+            plt.set_xlabel('Longitude')
+            plt.set_ylabel('Latitude')
 
             # Add coastlines
-            ax.coastlines()
+            coastlines()
 
             # Show the plot
             st.pyplot(fig2)
