15d5cdbd5a0514911888e21a1c004c16121fd90a
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-10 22:53:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.title('Rainfall Prediction - KMA')
 st.sidebar.title('Upload NetCDF File')
 uploaded_file = st.sidebar.file_uploader("Upload your NetCDF file", type=["nc"])
 
-show_cartopy_map = st.sidebar.checkbox("Subplot")
+show_cartopy_map = st.sidebar.checkbox("Un-check for larger Images", value=True)
 
 if uploaded_file is not None:
     # Load NetCDF data
